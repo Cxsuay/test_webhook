fd151bfbfdcea23a79541d59ef8993029503e6d4
guolin6 <guolin6@guolin6deMacBook-Pro.local>
2021-10-11 11:35:48 +0800

    msg

--- test-vue.sh
+++ test-vue.sh
@@ -8,11 +8,11 @@ git clean -f
 echo "拉取最新代码"
 git pull origin main
 echo "开始执行构建"
-docker build -t test-vue .
+docker build -t test-vue:1.0 .
 echo "停止并删除旧容器"
 docker stop test-vue-container
 docker rm test-vue-container
 echo "启动新容器"
 # 端口映射 宿主机 - 映射到 - 容器内容的 3000端口
 # -d 后台运行
-docker container run -p 3000:3000 --name test-vue-container -d test-vue
+docker container run -p 3000:3000 --name test-vue-container -d test-vue:1.0
